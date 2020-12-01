c8f100069f760ba5d35f4e48a1a8b670b3cbeee2
Cristi_Sima <cristiansimache@gmail.com>
2020-12-01 11:45:44 +0200

    added settings for ip & port added pesistece storage for ip & port added "show to server" to API

--- Android Client/Remote Input---Android Client/Assets/Scripts/Main.cs
+++ Android Client/Remote Input---Android Client/Assets/Scripts/Main.cs
@@ -4,68 +4,154 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Net.Sockets;
 using System.Text;
+using System;
+using System.IO;
 
 public class Main : MonoBehaviour
 {
-  // Start is called before the first frame update
-  public UIs uis;
-  private TcpClient socketConnection;
-  private string ip="192.168.0.200";
-  private int PORT=999;
-  void connect()
-  {
-    if(socketConnection==null)
-    {
-      try
-      {
-        Debug.Log("Trying to connect");
-        socketConnection = new TcpClient(ip, PORT);
-      }
-      catch (SocketException socketException)
-      {
-        Debug.Log("Socket exception: " + socketException);
-      }
-      Debug.Log("connected");
-    }
-  }
-  void Start()
-  {
-    Application.targetFrameRate = 60;
-    QualitySettings.vSyncCount = 1;
-    EventQueue=new List<string>();
-    uis.sync();
-    {
-    UIs.MAIN.Load();
-    // UIs.MAIN.Unload();
+	// Start is called before the first frame update
+	public UIs uis;
+	private TcpClient socketConnection;
 
-    }
-    // Debug.Log("SEND:AAA".Remove(0,5));
-    connect();
-    connect();
-  }
-  public static List<string> EventQueue;
-  // Update is called once per frame
-  private void Send(byte[] bytes)
-  {
-    //Encoding.ASCII.GetBytes(clientMessage);
-    NetworkStream stream = socketConnection.GetStream();
+	// settings
+	public static string ip {get; private set;}
+	public static int port {get; private set;}
+
+	private string settingsPath;
+	private int LinesInSettings=10;
+	void connect()
+	{
+		if(socketConnection!=null)
+		{
+			socketConnection.Close();
+			socketConnection=null;
+		}
+		try
+		{
+			Debug.Log("Trying to connect");
+			socketConnection = new TcpClient(ip, port);
+		}
+		catch (SocketException socketException)
+		{
+			Debug.Log("Socket exception: " + socketException);
+			return;
+		}
+		Debug.Log("connected");
+	}
+	void InitSettings()
+	{
+		File.Create(settingsPath).Dispose();
+
+		string[] settings=new string[LinesInSettings];
+		// IP
+		settings[0]="0.0.0.0";
+		// port
+		settings[1]="0";
+		File.WriteAllLines(settingsPath,settings);
+	}
+	void WriteSettings()
+	{
+		string[] settings=new string[LinesInSettings];
+		// IP
+		settings[0]=ip;
+
+		// port
+		settings[1]=port.ToString();
+		File.WriteAllLines(settingsPath,settings);
+	}
+	void ReadSettings()
+	{
+		string[] settings=new string[LinesInSettings];
+		settings=File.ReadAllLines(settingsPath);
+		// IP
+		ip=settings[0];
+
+		// port
+		port=Int32.Parse(settings[1]);
+
+
+	}
+	void Start()
+	{
+		settingsPath = Application.persistentDataPath + "/settings.txt";
+		Application.targetFrameRate = 60;
+		QualitySettings.vSyncCount = 1;
+		EventQueue=new List<string>();
+
+		if(!File.Exists(settingsPath))
+			InitSettings();
+		ReadSettings();
+
+		uis.sync();
+
+		UIs.MAIN.Load();
+		// UIs.MAIN.Unload();
+
+		connect();
+	}
+	public static void Show(string str)
+	{
+		Main.EventQueue.Add("SEND:SHOW_"+str);
+	}
+	public static List<string> EventQueue;
+	private void Send(byte[] bytes)
+	{
+		if(socketConnection==null)
+		{
+			connect();
+			if(socketConnection==null)
+				{
+					Debug.Log("DISCARDED");
+					return;
+				}
+		}
+		//Encoding.ASCII.GetBytes(clientMessage);
+		NetworkStream stream = socketConnection.GetStream();
 		stream.Write(bytes, 0, bytes.Length);
-  }
-  public static void Quit()
-  {
-    Debug.Log("EXIT");
-    Application.Quit();
-  }
-  void Update()
-  {
-    // connect();
-    if(EventQueue.Count>0)
-    {
-      if(EventQueue[0].Contains("SEND:"))
-      {
-        Send(Encoding.ASCII.GetBytes(EventQueue[0].Remove(0,5)+"|||"));
-        EventQueue.RemoveAt(0);
-      }
-    }
-  }
+	}
+	public static void Quit()
+	{
+		Debug.Log("EXIT");
+		// socketConnection.Close();
+		Application.Quit();
+	}
+	void Update()
+	{
+		// connect();
+		if(EventQueue.Count>0)
+		{
+			if(EventQueue[0].Substring(0,5)=="SEND:")
+			{
+				Send(Encoding.ASCII.GetBytes(EventQueue[0].Remove(0,5)+"|||"));
+				EventQueue.RemoveAt(0);
+			}
+			else if(EventQueue[0].Substring(0,7)=="UPDATE:")
+			{
+				if(EventQueue[0].Substring(0,7+5)=="UPDATE:PORT:")
+				{
+					port=Int32.Parse(EventQueue[0].Substring(7+5));
+				}
+				else if(EventQueue[0].Substring(0,7+3)=="UPDATE:IP:")
+				{
+					ip=EventQueue[0].Substring(7+3);
+				}
+				else
+				{
+					Debug.Log("UNKNOWN EVENT"+EventQueue[0]);
+					EventQueue.RemoveAt(0);
+					return;
+				}
+				connect();
+				WriteSettings();
+
+			}
+			else
+			{
+				Debug.Log("UNKNOWN EVENT"+EventQueue[0]);
+			}
+			EventQueue.RemoveAt(0);
+
+
+		}
+	}
 }
--- Android Client/Remote Input---Android Client/Assets/Scripts/TouchPad.cs
+++ Android Client/Remote Input---Android Client/Assets/Scripts/TouchPad.cs
@@ -6,90 +6,79 @@ using UnityEngine.UI;
 
 public class TouchPad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-      camera = Camera.main;
-    }
+	public Touch touch;
 
-    public Camera camera;
-    // Update is called once per frame
-    public Touch touch;
+	public Vector2 lastPos;
+	public Vector2 direction;
 
-    public Vector2 lastPos;
-    public Vector2 direction;
+	public int update_interval=2;
+	public int update_index=0;
+	void show(string s)
+	{
+		this.gameObject.transform.GetChild(0).GetComponent<Text>().text=s;
+	}
+	string V2tS(Vector2 v)
+	{
+		return "["+v.x.ToString()+","+v.y.ToString()+"]";
+	}
+	void gen_event()
+	{
+		direction=touch.position-lastPos;
+		lastPos=touch.position;
+		string e="SEND:INPUT_MOUSE_MOVE_"+V2tS(direction);
+		Main.EventQueue.Add(e);
+		// show(direction.magnitude.ToString());
+		// show(e);
 
-    public int update_interval=2;
-    public int update_index=0;
-    void show(string s)
-    {
-      this.gameObject.transform.GetChild(0).GetComponent<Text>().text=s;
-    }
-    string V2tS(Vector2 v)
-    {
-      return "["+v.x.ToString()+","+v.y.ToString()+"]";
-    }
-    void gen_event()
-    {
-      direction=touch.position-lastPos;
-      lastPos=touch.position;
-      string e="SEND:INPUT_MOUSE_MOVE_"+V2tS(direction);
-      Main.EventQueue.Add(e);
-      // show(direction.magnitude.ToString());
-      // show(e);
+	}
+	void Update()
+	{
+		show("NONE");
+		if (Input.touchCount > 0)
+		{
+			touch = Input.GetTouch(0);
+			Vector2 pos= this.gameObject.transform.position;
+			pos=Camera.main.WorldToScreenPoint(pos);
+			pos-=touch.position;
+			// Main.EventQueue.Add("SEND:SHOW_"+V2tS(pos));
+			// Main.EventQueue.Add("SEND:SHOW_"+V2tS(new Vector2(pos3.x,pos3.y)-touch.position-this.gameObject.GetComponent<RectTransform>().rect.size));
+			// Main.EventQueue.Add("SEND:SHOW_"+V2tS(this.gameObject.GetComponent<RectTransform>().rect.size));
+			// Main.EventQueue.Add("SEND:SHOW_"+V2tS(this.gameObject.transform.localScale));
+			Vector2 limits=Vector2.Scale(Vector2.Scale(this.gameObject.transform.localScale,this.gameObject.GetComponent<RectTransform>().rect.size),new Vector2(0.5f,0.5f));
+			// Main.EventQueue.Add("SEND:SHOW_"+V2tS(limits));
+			bool inside= Math.Abs(pos.x)<limits.x && Math.Abs(pos.y)<limits.y;
+			// Main.EventQueue.Add("SEND:SHOW_"+inside.ToString());
+			// Main.EventQueue.Add("SEND:SHOW_"+this.gameObject.GetComponent<RectTransform>().rect.width.ToString());
+			if(!inside) return;
+			if(touch.phase==TouchPhase.Began)
+			{
+				lastPos = touch.position;
+				// direction_last=new Vector2(0,0);
+			}
+			if(touch.phase==TouchPhase.Moved)
+			{
+				if(update_interval==update_index)
+				{
+					gen_event();
+					update_index=0;
+				}
+				else
+					update_index++;
+			}
+			if(touch.phase==TouchPhase.Ended)
+			{
+				gen_event();
+				update_index=0;
 
-    }
-    void Update()
-    {
-      show("NONE");
-      if (Input.touchCount > 0)
-      {
-        touch = Input.GetTouch(0);
-        Vector2 pos= this.gameObject.transform.position;
-        pos=camera.WorldToScreenPoint(pos);
-        pos-=touch.position;
-        // Main.EventQueue.Add("SEND:SHOW_"+V2tS(pos));
-        // Main.EventQueue.Add("SEND:SHOW_"+V2tS(new Vector2(pos3.x,pos3.y)-touch.position-this.gameObject.GetComponent<RectTransform>().rect.size));
-        // Main.EventQueue.Add("SEND:SHOW_"+V2tS(this.gameObject.GetComponent<RectTransform>().rect.size));
-        // Main.EventQueue.Add("SEND:SHOW_"+V2tS(this.gameObject.transform.localScale));
-        Vector2 limits=Vector2.Scale(Vector2.Scale(this.gameObject.transform.localScale,this.gameObject.GetComponent<RectTransform>().rect.size),new Vector2(0.5f,0.5f));
-        // Main.EventQueue.Add("SEND:SHOW_"+V2tS(limits));
-        bool inside= Math.Abs(pos.x)<limits.x && Math.Abs(pos.y)<limits.y;
-        // Main.EventQueue.Add("SEND:SHOW_"+inside.ToString());
-        // Main.EventQueue.Add("SEND:SHOW_"+this.gameObject.GetComponent<RectTransform>().rect.width.ToString());
-        if(!inside) return;
-        if(touch.phase==TouchPhase.Began)
-        {
-          lastPos = touch.position;
-          // direction_last=new Vector2(0,0);
-        }
-        if(touch.phase==TouchPhase.Moved)
-        {
-          if(true)//allways update
-            gen_event();
-          else
-            if(update_interval==update_index)
-            {
-              gen_event();
-              update_index=0;
-            }
-            else
-              update_index++;
-        }
-        if(touch.phase==TouchPhase.Ended)
-        {
-          gen_event();
-          update_index=0;
-
-        }
-      }
-    }
-    void OnMouseDrag()
-    {
-        if (Input.mousePosition.y < Screen.height / 2) {
-            // float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-            // transform.RotateAround (Vector2.up, -rotX);
-            Debug.Log(Input.GetAxis ("Mouse X"));
-        }
-    }
+			}
+		}
+	}
+	void OnMouseDrag()
+	{
+			if (Input.mousePosition.y < Screen.height / 2) {
+					// float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+					// transform.RotateAround (Vector2.up, -rotX);
+					Debug.Log(Input.GetAxis ("Mouse X"));
+			}
+	}
 }
